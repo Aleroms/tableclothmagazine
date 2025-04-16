d0835720bbe1e729e5c4b859487868a3e523b129
Santiago <santiagomorales333@yahoo.com>
2025-04-15 20:03:29 -0700

    added markdown for issue 2

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+        {
+            protocol: "https",
+            hostname: 'png.pngtree.com',
+            pathname: '/png-clipart/20231005/original/pngtree-watercolor-snowman-winter-doll-png-image_13124071.png'
+        }
+    ]
+  },
+};
 
 export default nextConfig;
