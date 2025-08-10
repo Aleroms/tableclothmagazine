0e3ed4112fa1cbeebab08493d7e9ccd0e8ab29ac
Dane Carstens <44176712+DanyeWestinator@users.noreply.github.com>
2025-08-10 14:21:18 -0700

    remove empty console log

--- app/lib/database/query.ts
+++ app/lib/database/query.ts
@@ -21,7 +21,6 @@ export const getShowcase = async (limit: number = 5) => {
           FROM showcases
           ORDER BY created_at DESC
           LIMIT ${limit}`;
-      console.log("");
       return data;
   } catch (error) {
     console.log(error);
