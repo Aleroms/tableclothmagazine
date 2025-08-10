0740136d63a4b4c7abfb7e1c471dad15f3ed58f3
Dane Carstens <44176712+DanyeWestinator@users.noreply.github.com>
2025-08-10 14:43:04 -0700

    split the difference on image sizing
    the 15 was chosen with the grid in mind - which we are not currently using ATM

--- app/ui/team/userCard.tsx
+++ app/ui/team/userCard.tsx
@@ -9,9 +9,9 @@ export default function UserCard({ user }: Props) {
   return (
     <div className="transition-all duration-200 ease-in-out rounded-3xl">
       {!user.img_url ? (
-        <div className="h-15 w-15 bg-stone-400 rounded-3xl md:h-20 md:w-20" />
+        <div className="h-25 w-25 bg-stone-400 rounded-3xl md:h-20 md:w-20" />
       ) : (
-        <div className="h-15 w-15 relative md:h-20 md:w-20">
+        <div className="h-25 w-25 relative md:h-20 md:w-20">
           <Image
             src={user.img_url}
             alt={`${user.first_name} ${user.last_name} profile picture`}
